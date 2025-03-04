d31bb5c303b566bfde767fef794cc2fa38ae59ac
hakan4773 <hb480312@gmail.com>
2025-03-04 17:32:05 +0300

    login apisi eklendi test edildi

---
+++ my-auth/app/api/login/route.ts
@@ -0,0 +1,32 @@
+import Auth from "../../models/auth";
+import connectDB from "../../libs/mongodb";
+import {User} from "../../types/user";
+import { NextRequest, NextResponse, userAgent } from "next/server";
+import bcryptjs from "bcryptjs";
+export async function POST(req:NextRequest) {
+await connectDB();
+try {
+const {email,password}= await req.json() as User;
+
+if(!email ||  !password){
+return NextResponse.json({message: "Please fill all fields"},{status:400});
+}
+
+const user=await Auth.findOne({email});
+
+if(!user){
+return NextResponse.json({message: "User does not exist"},{status:400});
+}
+
+const isMatch=await bcryptjs.compare(password,user.password);
+if(!isMatch){
+return NextResponse.json({message: "wrong password"},{status:400});
+}
+
+return NextResponse.json({message: "User logged in successfully"},{status:200});
+}
+catch (error: any) {
+return NextResponse.json({error: error.message},{status:400});
+}
+
+}
--- my-auth/app/api/register/route.ts
+++ my-auth/app/api/register/route.ts
@@ -2,16 +2,20 @@ import { NextRequest, NextResponse } from "next/server";
 import Auth from "../../models/auth";
 import connectDB from "../../libs/mongodb";
 import bcryptjs from "bcryptjs";
-
+import {User} from "../../types/user";
 export async function POST(req:NextRequest) {
     await connectDB();
     try {
-const {name,email,password}= await req.json();
+const {name,email,password}= await req.json() as User;
+if(!name || !email || !password){   
+    return NextResponse.json({message: "Please fill all fields"},{status:400});
+}
 
 const existingUser=await Auth.findOne({email});
 if(existingUser){
     return NextResponse.json({message: "User already exists"},{status:400});
 }
+
 const hashedPassword=await bcryptjs.hash(password,12);
     const newUser =  new Auth({name,email,password:hashedPassword});
     await newUser.save();
