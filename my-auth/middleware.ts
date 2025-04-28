39b8d26a0e760284afaf2bc5fefcf4efe5db11b4
hakan4773 <hb480312@gmail.com>
2025-04-28 23:15:48 +0300

    Refactor middleware to improve authentication flow and update route matcher

--- my-auth/middleware.ts
+++ my-auth/middleware.ts
@@ -6,12 +6,12 @@ export function middleware(req: NextRequest) {
     if (token && (req.nextUrl.pathname === "/login" || req.nextUrl.pathname === "/register"))
         { return NextResponse.redirect(new URL("/", req.url)); 
         }
-     if (!token && req.nextUrl.pathname.startsWith("/profile")) 
-        { return NextResponse.redirect(new URL("/login", req.url)); 
-
-        } return NextResponse.next(); 
+        if (!token && req.nextUrl.pathname !== "/login" && req.nextUrl.pathname !== "/register") {
+            return NextResponse.redirect(new URL("/login", req.url));
+        }
+         return NextResponse.next(); 
     } 
     export const config =
      {
-         matcher: ["/profile/:path*", "/login","/register"],
+         matcher: ["/profile/:path*", "/login","/register","/"],
      }
