c4275edb9cd81885f2d15b60cd1ba50fd0d14529
hakan4773 <hb480312@gmail.com>
2025-08-02 22:07:27 +0300

    login ve registerda navbar kaldırıldı

--- my-auth/app/layout.tsx
+++ my-auth/app/layout.tsx
@@ -1,16 +1,21 @@
-
+"use client";
 import Navbar from "./components/Navbar";
 import AuthProvider from "./context/AuthContext";
 import "./globals.css";
 import {ToastContainer } from "react-toastify";
 import Loading from "./loading";
 import { Suspense } from "react";
+import { usePathname } from "next/navigation";
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  
+    const pathname = usePathname();
+
+   const noNavbarPaths = ["/login", "/register"];
+   const showNavbar = !noNavbarPaths.includes(pathname);
+
   return (
     <html lang="en">
       <body>
@@ -18,7 +23,7 @@ export default function RootLayout({
          <AuthProvider> 
       <Suspense fallback={<Loading />}>
           
-        <Navbar />
+     {showNavbar && <Navbar />}
         <ToastContainer  position="top-right"  />
         {children}
       </Suspense>
